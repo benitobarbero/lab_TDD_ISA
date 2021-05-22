a03fe0d278797e76253a4cad7a5ec4032d6f690a
BuildTools <unconfigured@null.spigotmc.org>
2021-05-22 21:12:12 +0200

    Added template for operations

--- src/main/java/Operations.java
+++ src/main/java/Operations.java
@@ -1,2 +1,22 @@
 public class Operations {
+
+    public long suma(long a, long b){
+        return 0;
+    }
+
+    public long resta(long a, long b){
+        return 0;
+    }
+
+    public long multiplicacion(long a, long b){
+        return 0;
+    }
+
+    public double division(long a, long b){
+        return 0;
+    }
+
+    public double raiz(long a, long b){
+        return 0;
+    }
 }
