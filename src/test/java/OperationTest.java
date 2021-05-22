1675f31e0729e9675b0a1c0fb8366c1bd845d9a1
BuildTools <unconfigured@null.spigotmc.org>
2021-05-22 21:20:03 +0200

    Added tests for suma

--- src/test/java/OperationTest.java
+++ src/test/java/OperationTest.java
@@ -1,2 +1,34 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 public class OperationTest {
+    private Operations operations = new Operations();
+
+    @Test
+    public void testSuma(){
+        assertEquals(4, operations.suma(2,2));
+        assertEquals(10, operations.suma(4,6));
+        assertEquals(2000, operations.suma(-3000,5000));
+    }
+
+    @Test
+    public void testResta(){
+
+    }
+
+    @Test
+    public void testMultiplicacion(){
+
+    }
+
+    @Test
+    public void testDivision(){
+
+    }
+
+    @Test
+    public void testRaiz(){
+
+    }
 }
