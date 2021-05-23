3ce7187d70adc5103420145aa3508993099a3926
BuildTools <unconfigured@null.spigotmc.org>
2021-05-23 13:20:05 +0200

    Added raiz operation, now it works with tests

--- src/main/java/Operations.java
+++ src/main/java/Operations.java
@@ -1,23 +1,50 @@
 public class Operations {
 
-    public long suma(long a, long b){
-        return a+b;
+    public long suma(long a, long b) {
+        return a + b;
     }
 
 
-    public long resta(long a, long b){
-        return a-b;
+    public long resta(long a, long b) {
+        return a - b;
     }
 
-    public long multiplicacion(long a, long b){
-        return a*b;
+    public long multiplicacion(long a, long b) {
+        return a * b;
     }
 
-    public double division(long divisor, long dividendo){
-        return dividendo/divisor;
+    public double division(long divisor, long dividendo) {
+        return dividendo / divisor;
     }
 
-    public double raiz(long a, long b){
-        return 0;
+    public double raiz(long a, long b) {
+        long raizAproximada = calculoRaizAproximada(b);
+        double result = bakhshali(b,raizAproximada);
+        return result;
+    }
+
+    private long calculoRaizAproximada(long numCalcularRaiz) {
+        long raizAprox = 1;
+        long cuadrado = elevar(raizAprox, 2);
+        while (cuadrado < numCalcularRaiz) {
+            raizAprox++;
+            cuadrado = elevar(raizAprox, 2);
+        }
+        return raizAprox;
+    }
+
+    private double bakhshali(long numCalcularRaiz, long raizAprox) {
+        double result = ((elevar(raizAprox, 4)) + (6 * elevar(raizAprox, 2) * numCalcularRaiz) + (elevar(numCalcularRaiz, 2))) /
+                ((4 * elevar(raizAprox, 3)) + (4 * raizAprox * numCalcularRaiz));
+        return result;
+    }
+
+    private long elevar(long base, long exponente) {
+        long result = base;
+        while (exponente > 1) {
+            result = result * base;
+            exponente--;
+        }
+        return result;
     }
 }
